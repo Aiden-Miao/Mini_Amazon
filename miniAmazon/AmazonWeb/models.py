55cdb26bd6ff7a063976a7993601d9eaa5db1186
km480 <km480@duke.edu>
2020-04-22 12:20:49 -0400

    update db & finish webinteract

--- miniAmazon/AmazonWeb/models.py
+++ miniAmazon/AmazonWeb/models.py
@@ -4,17 +4,18 @@ from django.utils import timezone
 
 # Create youur models here.
 class Warehouse(models.Model):
-    #do we need wh_id?
-    wh_id = models.IntegerField()
     x_location = models.IntegerField()
     y_location = models.IntegerField()
     
 class Product(models.Model):
-    product_id = models.IntegerField()
     name = models.CharField(max_length = 200, default = "name")
     description = models.TextField(blank = True)
     price = models.DecimalField(max_digits = 9, decimal_places = 2)
 
+class WhInventory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    
 """
 class Items(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -31,21 +32,20 @@ class userprofile(User):
     myaddress_y = models.IntegerField()
     
 class Order(models.Model):
+    #the fields user generate
     create_time = models.DataTimeField()
-    #update_time = models.DataTimeField()
-    products = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    #total_price = models.DecimalField(max_digits = 9, decimal_places = 2)
-    order_id = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     dst_x = models.IntegerField()
     dst_y = models.IntegerField()
-    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    #the fields that server generate
+    warehouse_id = models.ForeignKey(Warehouse, on_delete = models.CASCADE)
+    is_processed = models.BooleanField()
     status = models.TextField(blank = True)
     
 class Truck(model.Model):
     truck_id = model.IntegerField()
-    #why?
     warehouse_id = models.ForeignKey(Warehouse)
     
-#what is purchase more?
--- miniAmazon/WebServer/webserver.py
+++ miniAmazon/WebServer/webserver.py
@@ -16,7 +16,8 @@ Ups_address = ("0.0.0.0", 34567)
 conn = psycopg2.connect(host = "vcm-12360.vm.duke.edu",database = "postgres", user = "postgres",port = "5433")
 global WORLD_SOCKET
 global Seq
-
+TOTALL_WHNUM = 3
+SPEED = 99999
 """
 --------------------basic function------------------
 """
@@ -61,22 +62,48 @@ def connect_world(socket, worldID):
         print("Amazon Connect to world:" + str(response.worldid) + " succeed!")
     else:
         print("Amazon Connect to world:" + str(response.worldid) + " fails!")
+
+#init, create the socket for world
+def init_world():
+    WORLD_SOCKET = create_socket(World_address)
         
 """
 ------------------------world function---------------------
 """
+#world handler
+def world_handler():
+    world_response = recv_message(WORLD_SOCKET, world_amazon.AResponses)
+    for errors in world_response.error:
+        print("The error is from command: ", errors.originseqnum)
+        print("The error is: ", errors.err)
+        
+    for allack in world_response.acks:
+        print("Receivce ack number: ", allack)
+
+    for arrive in world_response.arrived:
+        print("purchase more succeed, ready to pack!")
+        arrive_handler(arrive)
+
+    for packed in world_response.ready:
+        print("order packed, ready to load!")
+        packed_handler(packed)
+
+    for loaded in world_response.loaded:
+        print("order loaded, ready to let truck go!")
+        loaded_handler(loaded)
+
+    if world_response.finished == True:
+        print("Close world connection...")
+        WORLD_SOCKET.close()
+
+        
 #send_world_ack
 def send_world_ack(world_response):
     response = world.ACommands()
     response.acks.append(world_response.seqnum)
     send_msg(response, WORLD_SOCKET)
     
-#Purchase the product
-def purchase_product(purchasemore):
-    print("start puchasing product")
-    send_world_ack(purchasemore)
-    try:
-        cur = conn.cursor
+
 #pack the product
 def pack_product(whNum, product):
     command = world_amazon.ACommands()
@@ -92,16 +119,68 @@ def AMAZON_to_UPS(message):
     UPS_socket.close()
     return response
 
-#init, create the socket for world
-def init_world():
-    WORLD_SOCKET = create_socket(World_address)
-
 """
 ------------------------website function--------------------
 """
-def buy_on_website():
+#In this function, we keep search in the database, see if there is any order open. If it is open, then we start working on this order
+def web_handler():
     print("enter buy on website")
+    while(True):
+        try:
+            cur = conn.cursor()
+            cur.execute("SELECT id FROM AmazonWeb_order WHERE is_processed = False;")
+            unprocessed_order = cur.fetchall()
+            for orders in unprocessed_order:
+                order_id = orders[0]
+                #update the wharehouse
+                whnum = purchase_in_warehouse(amount,order_id)
+                cur.execute("UPDATE AmazonWeb_order SET warehouse_id = %s WHERE id =%s",(whnum, unique_id))
+                cur.execute("UPDATE AmazonWeb_order SET is_processed = %s",("True"))
+                tobuy(order_id)
+            conn.commit()
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error) 
+            
+#return wh number
+def purchase_in_warehouse(amount,order_id):
+    num = 1
+    return num
 
+#this function purchase more in warehouse
+def tobuy(order_id):
+    #whnum = order_id % TOTALL_WHNUM
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT id,products,quantity FROM AmazonWeb_order WHERE id = %s",(order_id))
+        row = cur.fetchone()
+        order_id = row[0]
+        amount = row[2]
+        cur.execute("SELECT name,description FROM AmazonWeb_Product WHERE id = %s",(row[1]))
+        detail = cur.fetchone()
+        name = detail[0]
+        description = detail[1]
+        command = world_amazon.ACommands()
+        command.simspeed = SPEED
+        BUY = command.buy.add()
+        allproducts = BUY.things.add()
+        allproducts.id = order_id
+        #maybe description can be the same as name?
+        allproducts.description = description
+        allpruducts.count = amount
+        send_message(WORLD_SOCKET, command)
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error) 
+"""
+#check the availability of a product
+def check_availability(orderid, amount):
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT products FROM ")
+"""
+    
 #main function. used to test
 def main():
     #amazon first creates the socket,then UPS connect to it. UPS connect to world then send worldid to amazon
@@ -121,5 +200,5 @@ def main():
 if __name__ == "__main__":
     thread1 = threading.Thread(target = world_handler, args = (,))
     thread2 = threading.Thread(target = ups_handler, args = (,))
-    thread3 = threading.Thread(target = world_handler, args = (,))
+    thread3 = threading.Thread(target = web_handler)
     main()
